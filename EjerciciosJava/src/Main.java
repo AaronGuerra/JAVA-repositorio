c222db0252d2ea3823163e4a6c5b43bb1f7ab108
AaronGuerra <thebo1992@gmail.com>
2026-06-02 23:07:41 -0400

    actualización archivos JAVA

--- EjerciciosJava/src/Main.java
+++ EjerciciosJava/src/Main.java
@@ -13,7 +13,7 @@ public class Main {
         System.out.println("Ingrese su año de nacimiento: ");
         int fechaNac = entrada.nextInt();
         int edadActual = 2026 - fechaNac;
-        System.out.println("Hola amigos, soy " + nombre + " tengo " + edadActual + " años porque nací en " + fechaNac);
+        System.out.println("Hola amigos, soy " + nombre + " tengo " + edadActual + " años, porque nací en " + fechaNac);
         System.out.println("-----------------------------------------------------");
 
         System.out.println("2 - Convertir temperaturas");
@@ -31,9 +31,47 @@ public class Main {
         double perimetro = (base + altura) * 2;
         double area = base * altura;
         System.out.println("El área es: " + area + " y el perímetro es " + perimetro);
+        System.out.println("-----------------------------------------------------");
 
+        System.out.println("4 - Calculadora de Descuento");
+        System.out.println("Ingrese un precio de un precio de un producto: ");
+        double precio = entrada.nextDouble();
+        System.out.println("Ingrese un descuento al producto: ");
+        double descuento = entrada.nextDouble();
+        double precioDes = precio * (descuento/100);
+        double total = precio - precioDes;
 
+        System.out.println("\n--- Resumen de Compra ---");
+        System.out.println("Precio Original: $" + precio);
+        System.out.println("Descuento Aplicado: $" + precioDes);
+        System.out.println("Total a pagar: $" + total);
+        System.out.println("-----------------------------------------------------");
 
+        System.out.println("5 - Condicionales");
+        System.out.println("Ingrese un número: ");
+        int numCond = entrada.nextInt();
+
+        if (numCond>0){
+            System.out.println("El número es Positivo");
+        }else if (numCond<0) {
+            System.out.println("El número es Negativo");
+        }else {
+            System.out.println("El número es Cero");
+        }
+        System.out.println("-----------------------------------------------------");
+
+        System.out.println("6 - Condicionales Edad");
+        System.out.println("Ingrese un número: ");
+        int numEdad = entrada.nextInt();
+
+        if (numEdad>=18){
+            System.out.println("Es mayor de edad");
+        }else if (numEdad<17) {
+            System.out.println("Es menor de edad");
+        }else{
+            System.out.println("Ingrese un número válido");
+
+        }
 
 
     }
